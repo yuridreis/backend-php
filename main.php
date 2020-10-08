5743e5b8252103bff011f73533a90263807a7af2
=Yuri Reis <=yuriazdreis@gmail.com>
2020-10-08 05:31:41 -0300

    Update main file

--- main.php
+++ main.php
@@ -2,8 +2,27 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Moovin\Job\Backend;
+use Moovin\Job\Backend as Conta;
 
-$exemplo = new Backend\Exemplo;
+$conta_corrente = new Conta\ContaCorrente();
+$conta_poupanca = new Conta\ContaPoupanca();
 
-echo $exemplo->exemplo();
+$conta_corrente->deposito(1000.00);
+
+$conta_poupanca->deposito(500.00);
+
+echo "<br> Valor da conta corrente = ". $conta_corrente->getSaldo();
+
+echo "<br> Valor da conta poupança = ". $conta_poupanca->getSaldo();
+
+
+$conta_corrente->transferencia(250, $conta_poupanca);
+
+echo "<br> Valor da conta corrente apos transferencia = ". $conta_corrente->getSaldo();
+echo "<br> Valor da conta poupança apos transferencia = ". $conta_poupanca->getSaldo();
+
+$conta_poupanca->saque(10);
+$conta_corrente->saque(10);
+
+echo "<br> Valor da conta corrente apos saque de 10 cybers = ". $conta_corrente->getSaldo();
+echo "<br> Valor da conta poupança apos saque de 10 cybers = ". $conta_poupanca->getSaldo();
